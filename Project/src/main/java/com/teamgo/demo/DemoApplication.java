9e02b4d1cca04d576716aa104a1b92f66fabf46e
李逸群 <315931676@qq.com>
2019-08-23 16:27:23 +0800

    web登录前后端交互

--- Project/src/main/java/com/teamgo/demo/DemoApplication.java
+++ Project/src/main/java/com/teamgo/demo/DemoApplication.java
@@ -3,13 +3,19 @@ package com.teamgo.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.teamgo.demo.controller.controller","com.teamgo.demo.service"})
 @MapperScan(basePackages = "com.teamgo.demo.mapper")
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
 
+        return application.sources(DemoApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
